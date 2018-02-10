3067715829c5fc3772caeac78d69e66a25a47770
Samuel Kitono <samuelkitono@skitono-personal-mac.group.on>
2018-02-10 02:18:04 -0800

    Removes unnecessary code

--- SlackInterview/Service/APIRequest.swift
+++ SlackInterview/Service/APIRequest.swift
@@ -9,9 +9,7 @@
 import Foundation
 
 /// All requests must conform to this protocol
-/// - Discussion: You must conform to Encodable too, so that all stored public parameters
-///   of types conforming this protocol will be encoded as parameters.
-public protocol APIRequest: Encodable {
+public protocol APIRequest {
     /// Response (will be wrapped with a DataContainer)
     associatedtype Response: Decodable
     
--- SlackInterview/Service/GetSlackUsers.swift
+++ SlackInterview/Service/GetSlackUsers.swift
@@ -14,21 +14,4 @@ public struct GetSlackUsers: APIRequest {
     public var resourceName: String {
         return "users.list"
     }
-        
-    // Parameters
-    public let name: String?
-    public let nameStartsWith: String?
-    public let limit: Int?
-    public let offset: Int?
-    
-    // Note that nil parameters will not be used
-    public init(name: String? = nil,
-                nameStartsWith: String? = nil,
-                limit: Int? = nil,
-                offset: Int? = nil) {
-        self.name = name
-        self.nameStartsWith = nameStartsWith
-        self.limit = limit
-        self.offset = offset
-    }
 }
